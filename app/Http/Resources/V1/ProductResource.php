911df27b56028c1839a28c3a379890598ed63eee
nopiarahman <nopiarahman93@gmail.com>
2022-07-03 13:34:00 +0700

    add image on productResource

--- app/Http/Resources/V1/ProductResource.php
+++ app/Http/Resources/V1/ProductResource.php
@@ -12,12 +12,21 @@ class ProductResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
+    public function cekImages()
+    {
+        $file=[];
+        foreach($this->image as $image){
+            $file[]=$image->file;
+        }
+        return $file;
+    }
     public function toArray($request)
     {
         return [
             'id'=>$this->id,
             'name'=>$this->name,
-            'description'=>$this->description
+            'description'=>$this->description,
+            'images'=>$this->cekImages()
         ];
     }
 }
